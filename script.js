267145aaba9d57d35f0f5b2f32447233245d8730
Bruno <juegos.malafar@gmail.com>
2023-03-27 23:21:33 -0300

    Se emprolija codigo de la entrega

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 class ProductManager {
   constructor() {
     this.products = [];
-    this.latestId = 1;
+    this.nextId = 1;
   }
   addProduct(...product) {
 
     product.forEach((p) => {
       const hasSameCode = this.products.find((x) => x.code === p.code);
-      const productData = Object.values(p);
+      const missingData = Object.values(p).includes(undefined);
 
-      if (hasSameCode || productData.includes(undefined)) {
-        console.log(`Al producto ${p.title} le faltan datos / El codigo de producto ya existe`);
-      } else {
-        p.id = this.latestId;
-        this.latestId++;
+      if (missingData) console.log(`Al producto ${p.title} le faltan datos.`);
+      
+      if (hasSameCode) console.log(`El codigo del producto ${p.title} ya existe`); 
+      
+      if (!missingData && !hasSameCode){
+        p.id = this.nextId;
         this.products.push(p);
+        this.nextId++;
       }
     });
   }
@@ -32,7 +34,7 @@ class ProductManager {
 
 class Product {
   constructor(title, description, price, thumbnail, code, stock) {
-    (this.title = title),
+      (this.title = title),
       (this.description = description),
       (this.price = price),
       (this.thumbnail = thumbnail),
@@ -46,11 +48,11 @@ const products = new ProductManager();
 const product1 = new Product("Celular", "Telefonia", 50000, "url...", 400, 10);
 const product2 = new Product("Auricular", "Audio", 8000, "url...", 500, 7);
 const product3 = new Product("TV 50 pulgadas", "TV", 100000, "url...", 400, 15); // se repite el mismo code que el primer producto para forzar error
-const product4 = new Product("Notebook banghoo", "Computacion", 200000, "url...", 600, 5);
+const product4 = new Product("Notebook banghoo", "Computacion", 200000, "url...", 700, 5);
 
 products.addProduct(product1, product2, product3, product4);
 
 
-//  products.getProducts();
-//  products.getProductById(1);
-//  products.getProductById(5);
+// products.getProducts();
+// products.getProductById(1);
+// products.getProductById(5);
